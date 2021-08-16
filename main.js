af06f458507bd6fd221dcc14786e5afec3094ee1
shuboo31 <79737591+shuboo31@users.noreply.github.com>
2021-08-16 16:27:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.image.from.URL('Brithdayimage.jpg', function(image){
+    block_image_Object= img
+
+    block_image_Object.scaleToWidth(700);
+    block_image_Object.scaleToHeight(510)
+    block_image_Object.set({
+	top:0
+    left:0
+    });
+
+
+function playSound(){
+	
+}
